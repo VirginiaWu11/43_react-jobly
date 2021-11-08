65c27adc48895ea6e7bb8263596394563575f111
Virginia Wu <virginiawu11@gmail.com>
2021-11-07 22:18:05 -0500

    add api methods

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -21,9 +21,7 @@ class JoblyApi {
     //this has been provided to show you another way to pass the token. you are only expected to read this code for this project.
     const url = `${BASE_URL}/${endpoint}`;
     const headers = { Authorization: `Bearer ${JoblyApi.token}` };
-    const params = (method === "get")
-        ? data
-        : {};
+    const params = method === "get" ? data : {};
 
     try {
       return (await axios({ url, method, data, params, headers })).data;
@@ -43,10 +41,51 @@ class JoblyApi {
     return res.company;
   }
 
-  // obviously, you'll add a lot here ...
+  /** Get all companies. */
+  static async getCompanies() {
+    let res = await this.request(`companies/`);
+    return res.companies;
+  }
+
+  /** Get all jobs. */
+  static async getJobs() {
+    let res = await this.request(`jobs/`);
+    return res.companies;
+  }
+
+  /** Log in. */
+  static async logIn(username = "testuser", password = "password") {
+    let res = await this.request(
+      `auth/token`,
+      {
+        user: {
+          username: username,
+          password: password,
+        },
+      },
+      "post"
+    );
+    return res.token;
+  }
+
+  /** Sign up. */
+  static async register(username, password) {
+    let res = await this.request(
+      `auth/register`,
+      {
+        user: {
+          username: username,
+          password: password,
+        },
+      },
+      "post"
+    );
+    return res.token;
+  }
 }
 
 // for now, put token ("testuser" / "password" on class)
-JoblyApi.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ" +
-    "SI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0." +
-    "FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc";
+JoblyApi.token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ" +
+  "SI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0." +
+  "FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc";
