bfec6c40ac3e79f69a14796827ec356078f45add
Virginia Wu <virginiawu11@gmail.com>
2021-11-07 22:50:59 -0500

    add routes and components skeleton

---
+++ frontend/src/Routes.js
@@ -0,0 +1,39 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from "react";
+import Home from "./Home";
+import JobList from "./JobList";
+import CompanyList from "./CompanyList";
+import LoginForm from "./LoginForm";
+import SignupForm from "./SignupForm";
+import ProfileForm from "./ProfileForm";
+
+<BrowserRouter>
+  {/* <NavBar /> */}
+
+  <Switch>
+    <Route exact path="/">
+      <Home />
+    </Route>
+    <Route exact path="/companies/:name">
+      <JobList jobs={jobs} />
+    </Route>
+    <Route exact path="/companies">
+      <CompanyList companies={companies} />
+    </Route>
+    <Route exact path="/jobs">
+      <JobList jobs={jobs} />
+    </Route>
+    <Route exact path="/login">
+      <LoginForm />
+    </Route>
+    <Route exact path="/signup">
+      <SignupForm />
+    </Route>
+    <Route exact path="/profile">
+      <ProfileForm />
+    </Route>
+    <Route>
+      <p>not found</p>
+    </Route>
+  </Switch>
+</BrowserRouter>;
