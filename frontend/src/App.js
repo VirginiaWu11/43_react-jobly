243467edc3dddc3d90bfb6431e3bec8a7e45e988
Virginia Wu <virginiawu11@gmail.com>
2021-11-18 21:36:09 -0500

    add links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 
 import AllRoutes from "./AllRoutes";
+import NavBar from "./NavBar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <NavBar />
         <AllRoutes />
       </BrowserRouter>
     </div>
---
+++ frontend/src/NavBar.js
@@ -0,0 +1,38 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Button component={Link} to="/" color="inherit">
+            Jobly
+          </Button>
+        </Typography>
+        <Button component={Link} to="/jobs" color="inherit">
+          Jobs
+        </Button>
+        <Button component={Link} to="/companies" color="inherit">
+          Companies
+        </Button>
+        <Button component={Link} to="/profile" color="inherit">
+          Profile
+        </Button>
+        <Button component={Link} to="/logout" color="inherit">
+          Logout
+        </Button>
+        <Button component={Link} to="/login" color="inherit">
+          Login
+        </Button>
+        <Button component={Link} to="/signup" color="inherit">
+          Sign Up
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
