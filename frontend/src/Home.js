b3e5c6ebcec0965a01f4b5c6a62643bb45bfd557
Virginia Wu <virginiawu11@gmail.com>
2021-11-22 20:54:56 -0500

    add logic to show signed in/out text

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,12 +1,78 @@
+import React, { useContext } from "react";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Link } from "react-router-dom";
+import UserContext from "./UserContext";
 
-const Home = () => {
+const theme = createTheme();
+
+export default function Home() {
+  const { currentUser } = useContext(UserContext);
   return (
-    <div>
-      <h1>Home</h1>
-      <Link to="/companies">Companies</Link>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: "url(https://source.unsplash.com/random)",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography component="h1" variant="h2">
+              Jobly
+            </Typography>
+            <br />
+            <Typography component="h2" variant="h5">
+              All the jobs in one, convenient place.
+            </Typography>
+            {currentUser ? (
+              <Typography component="h3" variant="h3">
+                Welcome Back, {currentUser.firstName || currentUser.username}
+              </Typography>
+            ) : (
+              <Box noValidate>
+                <Button
+                  component={Link}
+                  to="/signin"
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2, mx: 2 }}
+                >
+                  Sign In
+                </Button>
+                <Button
+                  component={Link}
+                  to="/signup"
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2, mx: 2 }}
+                >
+                  Sign Up
+                </Button>
+              </Box>
+            )}
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
-};
-
-export default Home;
+}
