63ca02ccea78e21906efbeae0130e7de071e8095
Virginia Wu <virginiawu11@gmail.com>
2021-11-25 15:03:21 -0500

    add apply button

--- frontend/src/JobCard.js
+++ frontend/src/JobCard.js
@@ -1,16 +1,22 @@
-import * as React from "react";
+import React, { useState, useContext } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import { Grid } from "@mui/material";
+import UserContext from "./UserContext";
 
 const JobCard = ({ job }) => {
+  const { hasAppliedToJob, applyToJob } = useContext(UserContext);
+  const [applied, setApplied] = useState();
+
   return (
-    <Card sx={{ minWidth: 275, display: "flex" }}>
+    <Card sx={{ minWidth: 275 /*  display: "flex" */ }}>
       <Box
         sx={{
           display: "flex",
-          flexDirection: "column",
+          // /* flexDirection: "column", */
         }}
       >
         <CardContent>
@@ -24,6 +30,17 @@ const JobCard = ({ job }) => {
           <Typography variant="body2">{`Equity: $${job.equity}`}</Typography>
         </CardContent>
       </Box>
+      <Grid container justifyContent="flex-end">
+        <Button
+          variant="contained"
+          // onClick={handleApply}
+          size="large"
+          disabled={applied}
+          sx={{ mb: 2, mr: 2 }}
+        >
+          {applied ? "Applied" : "Apply"}
+        </Button>
+      </Grid>
     </Card>
   );
 };
